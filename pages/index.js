9bf2cbbeeb9335085a4bb39ca4f5e0673b8426de
Brandon Lui <2176046-blui@users.noreply.gitlab.com>
2022-09-29 17:07:28 +0000

    Update pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 class="gradienttext super defaultcolor" className={styles.title}>
           Brandon Lui
         </h1>
 
