5524673bf69f4a29413bbdff11203992bd94cd17
Brandon Lui <>
2022-11-07 14:54:14 -0500

    change link

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header>
  <h1 class="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-red-600 ">
-         <a href="/"> Brandon Lui </a>
+         <Link href="/"> Brandon Lui </Link>
         </h1>
         </header>
     )
