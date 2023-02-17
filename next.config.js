36e07654be8e7e6bcc6bead8f65d90a69bfeea22
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2023-02-17 15:02:19 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/instagram',
+        destination: 'https://www.instagram.com/brandonluii/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
