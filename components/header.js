2581de334d547a68369609322af47a187fdc9f62
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2024-11-12 17:13:42 -0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <header>
- <h1 class="text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-red-600 ">
+ <h1 class="name text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-red-600 ">
          <Link href="/"> Brandon Lui </Link>
         </h1>
         </header>
