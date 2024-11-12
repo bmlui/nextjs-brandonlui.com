a6c2f27b34afee666daa5513f3788ceedc66716a
Brandon Lui <17382500+bmlui@users.noreply.github.com>
2024-11-12 17:25:19 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -9,9 +9,9 @@ function MyApp({ Component, pageProps }) {
   <Head>
         <title>Brandon Lui</title>
         <meta name="description" content="I'm a student at Worcester Polytechnic Institute studying Computer Science." />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&family=Scheherazade+New:wght@400;500;600;700&display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Text:ital@0;1&display=swap" rel="stylesheet" />
       </Head>
     <main className='min-h-screen py-6 px-6'> 
     <Header />
